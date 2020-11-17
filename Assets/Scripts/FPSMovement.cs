718be693dc77d94289904690e24a1cfb08ed0ffa
Connor <connorscott128@gmail.com>
2020-11-17 15:04:41 -0600

    Added jump physics when on platform, player follows platform when jump
    Can be changed later, this adds some kind of realism that may not be wanted or needed. Not ideal at the moment but it works.

--- Assets/Scripts/FPSMovement.cs
+++ Assets/Scripts/FPSMovement.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 
 public class FPSMovement : MonoBehaviour
 {
@@ -9,7 +6,9 @@ public class FPSMovement : MonoBehaviour
                  jumpSpeed = 8.0f,
                  runSpeed = 8.0f,
                  gravity = 20.0f;
-    public Vector3 moveDirection = Vector3.zero;
+        
+    public Vector3 moveDirection = Vector3.zero,
+        jumpDirection;
 
     static CharacterController controller;
 
@@ -26,13 +25,12 @@ public class FPSMovement : MonoBehaviour
             moveDirection = Camera.main.transform.TransformDirection(moveDirection);
             moveDirection.y = 0.0f; //important for not 'jumping' when looking up
             moveDirection *= walkSpeed;
-
+            if (this.GetComponentInParent<PlaneMovement>())
+                jumpDirection = this.GetComponentInParent<PlaneMovement>().FrameVelocity * Time.deltaTime;
+            else
+                jumpDirection = Vector3.zero;
             if (Input.GetButton("Jump"))
             {
-                if (!controller.isGrounded)
-                {
-                    moveDirection = moveDirection + this.transform.TransformDirection(moveDirection);
-                }
                 moveDirection.y = jumpSpeed;
             }
             if (Input.GetKey(KeyCode.LeftShift))
@@ -40,14 +38,12 @@ public class FPSMovement : MonoBehaviour
                 moveDirection /= walkSpeed;
                 moveDirection *= runSpeed;
             }
-
         }
-
+        if(jumpDirection != Vector3.zero)
+        {
+            moveDirection += jumpDirection;
+        }
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
-
-
     }
-
-    
 }
--- Assets/Scripts/PlaneMovement.cs
+++ Assets/Scripts/PlaneMovement.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class PlaneMovement : MonoBehaviour
 {
     public enum axisTransform { Forward, Right, Up, Down, Left, Back, Zero };
-    GameObject Plane;
-    public float Amplitude = 2.0f;
-    public float Speed = 2.0f;
-    public float changeTime = 2.0f;
-    private Vector3[] vectorAxis = new Vector3[7];
     public axisTransform axisOfTransform;
-    public Vector3 velocity, lastPos, currentPos;
-   
+    public float Amplitude = 2.0f,
+                 Speed = 2.0f,
+                 changeTime = 2.0f;
+    private Vector3[] vectorAxis = new Vector3[7];
+    public Vector3 currentPos;
+    public Vector3 FrameVelocity { get; set; }
+    public Vector3 PrevPosition { get; set; }
+    GameObject Plane;
 
     // Start is called before the first frame update
     void Start()
@@ -26,22 +27,21 @@ public class PlaneMovement : MonoBehaviour
         vectorAxis[5] = Vector3.back;
         vectorAxis[6] = Vector3.zero;
     }
-
     // Update is called once per frame
     void FixedUpdate()
     {
         Plane.transform.Translate(Amplitude * GetAxis(axisOfTransform) * Mathf.Sin(((2 * Time.timeSinceLevelLoad) / changeTime) * Speed) * Time.fixedDeltaTime);
-        currentPos = Plane.transform.position;
-        velocity = lastPos - currentPos;
-        lastPos = currentPos;
     }
 
+    public void Update()
+    {
+        Vector3 currFrameVelocity = (transform.position - PrevPosition) / Time.deltaTime;
+        FrameVelocity = Vector3.Lerp(FrameVelocity, currFrameVelocity, 0.1f); //lerping the velocity to avoid single frame irregularities
+        PrevPosition = transform.position;
+    }
     public Vector3 GetAxis(axisTransform axis)
     {
         return vectorAxis[(int)axis];
     }
-    public Vector3 getVelocity()
-    {
-        return velocity;
-    }
+
 }
